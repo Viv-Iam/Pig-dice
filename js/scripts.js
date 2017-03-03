eab12a81cbd3956d76d128f2c2a21271154b03a5
Viv-Iam <vivian.opondoh@gmail.com>
2017-03-03 15:56:11 +0300

    added the roll and hold buttons

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+//BUISNESS LOGIC//
+var pone="";
+var ptwo="";
+
+//GENERATES RANDOM NUMBERS//
+var rollDie = function () {
+  return Math.floor(6*Math.random())+1;
+}
+
+//OBJECT INITIAL PLAY//
+function PigD(initial) {
+  this.roll = 0;
+  this.roundscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.pName;
+}
+
+//WHEN PLAYER ROLLS 1//
+PigD.prototype.rollone = function() {
+  if (this.roll === 1) {
+  this.roundscore = 0;
+  alert(this.pName + ", you scored 0 Your turn is over!")
+
+  //IF ROLL != 1 PLAYER KEEPS PLAYING//
+} else {
+this.roundscore += this.roll;
+}
+}
+
+//WHEN PLAYER CHOOSES TO HOLD//
+PigD.prototype.hold = function () {
+  this.totalscore += this.roundscore;
+
+  //ONCE HAVE SCORE ADDED ROUNDSCORE MADE 0 AGAIN//
+  this.roundscore = 0;
+  alert(this.pName + ", your turn is over");
+
+  //WHEN PLAYER SCORES 100//
+  Player.prototype.scorehundred = function () {
+    if (this.totalscore >= 100) {
+      alert(this.pName + " is the WINNER!");
+    }
+  }
+
+  //TO PLAY AGAIN//
+  Player.prototype.playAgain = function () {
+    this.roll = 0;
+    this.roundscore = 0;
+    this.totalscore = 0;
+    this.pName ="";
+  }
+  var clearValues = function(){
+    $(".player1").val("");
+    $(".player2").val("");
+  }
+  //USER INTERFACE//
+  
