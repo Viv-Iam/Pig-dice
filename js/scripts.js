4806bfdbe4c0f6c13cf7d610c536dc9d83f6d192
Viv-Iam <vivian.opondoh@gmail.com>
2017-03-05 22:51:58 +0300

    made changes in id's and made js work

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ function PigD(initial) {
   this.roll = 0;
   this.roundscore = 0;
   this.totalscore = 0;
-  this.turn = turn;
+  this.turn = initial;
   this.pName;
 }
 
@@ -35,7 +35,7 @@ PigD.prototype.hold = function () {
   //ONCE HAVE SCORE ADDED ROUNDSCORE MADE 0 AGAIN//
   this.roundscore = 0;
   alert(this.pName + ", your turn is over");
-
+}
   //WHEN PLAYER SCORES 100//
   PigD.prototype.scorehundred = function () {
     if (this.totalscore >= 100) {
@@ -51,25 +51,25 @@ PigD.prototype.hold = function () {
     this.pName ="";
   }
   var clearValues = function(){
-    $(".input1").val("");
-    $(".input2").val("");
+    $("firstplayer").val("");
+    $(".secondplayer").val("");
   }
   //USER INTERFACE//
   $(document).ready(function() {
   $("button#play").click(function(event){
-    pone = new Player(true);
-    ptwo =  new Player(false);
+    pone = new PigD(true);
+    ptwo =  new PigD(false);
     $(".game").show();
     $(".nameinput").hide();
 
     var playerone = $(".input1").val();
-    $("#span-pone").text(playerone);
+    $("#firstplayer").text(playerone);
 
     var playertwo = $(".input2").val();
-    $("#span-ptwo").text(playertwo);
+    $("#secondplayer").text(playertwo);
 
-    pone.pName=playerone;
-    ptwo.pName=playertwo;
+    pone.pName=firstplayer;
+    ptwo.pName=secondplayer;
     });
 
     $("button#play-again").click(function(event){
@@ -77,43 +77,43 @@ PigD.prototype.hold = function () {
       clearValues();
       pone.playAgain();
       ptwo.playAgain();
-      $("#turntotal1").empty();
-      $("#totalscore1").empty();
-      $("#roll-1").empty();
-      $("#turntotal2").empty();
-      $("#totalscore2").empty();
-      $("#roll-2").empty();
+      $("#turntotal-1").empty();
+      $("#totalscore-1").empty();
+      $("#rolldie-1").empty();
+      $("#turntotal-2").empty();
+      $("#totalscore-2").empty();
+      $("#rolldie-2").empty();
 
       $(".nameinput").show();
     });
 
-    $("button#roll1").click(function(event){
+    $("button#roll-1").click(function(event){
       pone.roll = rollDie();
-      $("#roll-1").text(pone.roll);
+      $("#rolldie-1").text(pone.roll);
       pone.rollone();
-      $("#turntotal1").text(pone.roundscore);
+      $("#turntotal-1").text(pone.roundscore);
     });
 
-    $("button#roll2").click(function(event){
+    $("button#roll-2").click(function(event){
       ptwo.roll = rollDie();
-      $("#roll-2").text(ptwo.roll);
+      $("#rolldie-2").text(ptwo.roll);
       ptwo.rollone();
-      $("#turntotal2").text(ptwo.roundscore);
+      $("#turntotal-2").text(ptwo.roundscore);
     });
 
-    $("button#hold1").click(function(event){
+    $("button#hold-1").click(function(event){
       pone.hold();
-      $("#totalscore1").text(pone.totalscore);
-      $("#turntotal1").empty();
-      $("#roll-1").empty();
+      $("#totalscore-1").text(pone.totalscore);
+      $("#turntotal-1").empty();
+      $("#rolldie-1").empty();
       pone.scorehundred();
     });
 
-    $("button#hold2").click(function(event){
+    $("button#hold-2").click(function(event){
       ptwo.hold();
-      $("#totalscore2").text(ptwo.totalscore);
-      $("#turntotal2").empty();
-      $("#roll-2").empty();
+      $("#totalscore-2").text(ptwo.totalscore);
+      $("#turntotal-2").empty();
+      $("#rolldie-2").empty();
       ptwo.scorehundred();
     });
 
